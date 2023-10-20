ec8664d51dad360be9ba8bdf0d3e2fdf990452ba
Éderson Cássio <edersoncassio1983@gmail.com>
2023-10-20 17:00:41 -0300

    Processador de mensagens -- já invocando o usecase de registrar transação

--- codeflix/application/kafka/process.go
+++ codeflix/application/kafka/process.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"time"
 
+	"derso.com/imersao-fullcycle/codepix-go/application/dto"
+	"derso.com/imersao-fullcycle/codepix-go/application/factory"
+	"derso.com/imersao-fullcycle/codepix-go/domain/model"
 	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	"gorm.io/gorm"
 )
@@ -41,7 +44,59 @@ func (k *KafkaProcessor) Consume() {
 		timeout := time.Duration(-1)
 		msg, err := consumer.ReadMessage(timeout)
 		if err == nil {
-			fmt.Println("Message received:", string(msg.Value))
+			k.processMessage(msg)
 		}
 	}
+}
+
+func (k *KafkaProcessor) processMessage(msg *ckafka.Message) {
+	transactionsTopic := "transactions" // TODO em variável de ambiente
+	transactionConfirmationTopic := "transaction-confirmation"
+
+	switch topic := *msg.TopicPartition.Topic; topic {
+	case transactionsTopic:
+		k.processTransaction(msg)
+	case transactionConfirmationTopic:
+	default:
+		fmt.Println("not a valid topic", topic, "msg:", string(msg.Value))
+	}
+}
+
+func (k *KafkaProcessor) processTransaction(msg *ckafka.Message) error {
+	transactionDto, err := dto.NewTransactionDTO(msg.Value)
+	if err != nil {
+		return err
+	}
+
+	transactionUseCase := factory.TransactionUseCaseFactory(k.Database)
+	createdTransaction, err := transactionUseCase.Register(
+		transactionDto.AccountID,
+		transactionDto.Amount,
+		transactionDto.PixKeyTo,
+		transactionDto.PixKeyKindTo,
+		transactionDto.Description,
+	)
+
+	if err != nil {
+		fmt.Println("error registering transaction", err)
+		return err
+	}
+
+	destinationBankTopic := "bank" + createdTransaction.PixKeyTo.Account.Bank.Code
+	transactionDto.ID = createdTransaction.ID
+	transactionDto.Status = model.TransactionPending
+	transactionJson, err := transactionDto.ToJson()
+
+	if err != nil {
+		fmt.Println("error generating JSON from transaction", err)
+		return err
+	}
+
+	err = Publish(string(transactionJson), destinationBankTopic, k.Producer, k.DeliveryChannel)
+	if err != nil {
+		fmt.Println("error sending message to destination bank", err)
+		return err
+	}
+
+	return nil
 }
