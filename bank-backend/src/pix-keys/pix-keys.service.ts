475a3a607c28c8364f930c400fcb3e975aae4169
Éderson Cássio <edersoncassio1983@gmail.com>
2023-10-21 17:00:37 -0300

    DTO e repo

--- bank-backend/src/pix-keys/pix-keys.service.ts
+++ bank-backend/src/pix-keys/pix-keys.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePixKeyDto } from './dto/create-pix-key.dto';
 import { UpdatePixKeyDto } from './dto/update-pix-key.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { PixKey } from './entities/pix-key.entity';
 
 @Injectable()
 export class PixKeysService {
+  constructor(
+    @InjectRepository(PixKey)
+    private pixKeyRepo: Repository<PixKey>,
+  ) {}
+
   create(createPixKeyDto: CreatePixKeyDto) {
     return 'This action adds a new pixKey';
   }
