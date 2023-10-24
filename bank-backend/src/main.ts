d602b0d41c70f2f7fc894f13ceac205de6b228ad
Éderson Cássio <edersoncassio1983@gmail.com>
2023-10-24 13:26:28 -0300

    Class-validator

--- bank-backend/src/main.ts
+++ bank-backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PixKeyAlreadyExistsFilter } from './pix-keys/filter/pix-key-already-exists.error';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new PixKeyAlreadyExistsFilter());
+  app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422 }));
   await app.listen(3000);
 }
 bootstrap();
