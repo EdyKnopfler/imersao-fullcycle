4803887ed2898cbf1611d5d8ec3d552ae9222a38
Éderson Cássio <edersoncassio1983@gmail.com>
2023-10-25 20:11:51 -0300

    Cliente Kafka para uso pelo serviço

--- bank-backend/src/transactions/transactions.module.ts
+++ bank-backend/src/transactions/transactions.module.ts
@@ -5,9 +5,27 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Transaction } from './entities/transaction.entity';
 import { BankAccount } from 'src/bank-accounts/entities/bank-account.entity';
 import { PixKey } from 'src/pix-keys/entities/pix-key.entity';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transaction, BankAccount, PixKey])],
+  imports: [
+    TypeOrmModule.forFeature([Transaction, BankAccount, PixKey]),
+    ClientsModule.registerAsync([
+      {
+        name: 'KAFKA_SERVICE',
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.KAFKA,
+          options: {
+            client: {
+              brokers: [configService.get('KAFKA_BROKER')],
+            },
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
+  ],
   controllers: [TransactionsController],
   providers: [TransactionsService],
 })
