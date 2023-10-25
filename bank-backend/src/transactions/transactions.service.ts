d6716e128bd6ba4f857e29af1144a5aea4184531
Éderson Cássio <edersoncassio1983@gmail.com>
2023-10-25 20:32:24 -0300

    Um gostinho de transação com banco de dados com lock e envio do evento para o broker

--- bank-backend/src/transactions/transactions.service.ts
+++ bank-backend/src/transactions/transactions.service.ts
@@ -1,17 +1,77 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
 import { CreateTransactionFromAnotherBankAccountDto } from './dto/create-transaction-from-another-bank-account.dto';
 import { ConfirmTransactionDto } from './dto/confirm-transaction.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Transaction, TransactionOperation } from './entities/transaction.entity';
+import { DataSource, Repository } from 'typeorm';
+import { BankAccount } from 'src/bank-accounts/entities/bank-account.entity';
+import { ClientKafka } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class TransactionsService {
-  create(createTransactionDto: CreateTransactionDto) {
-    return 'This action adds a new transaction';
+  constructor(
+    @InjectRepository(Transaction)
+    private transactionRepo: Repository<Transaction>,
+    @InjectRepository(BankAccount)
+    private bankAccountRepo: Repository<BankAccount>,
+    private dataSource: DataSource,
+    @Inject('KAFKA_SERVICE')
+    private kafkaService: ClientKafka,
+  ) {}
+
+  async create(
+    bankAccountId: string,
+    createTransactionDto: CreateTransactionDto,
+  ) {
+    // DataSource.transaction refere-se a uma transação de banco de dados
+    // A variável que recebe o retorno é uma entidade Transaction :P
+    const transaction = await this.dataSource.transaction(async (manager) => {
+
+      // Como vamos carregar o BankAccount para a memória antes de alterar o saldo,
+      // é fundamental realizar um lock na linha.
+      // Outras requisições sobre a mesma conta serão forçadas a esperar.
+      const bankAccount = await manager.findOneOrFail(BankAccount, {
+        where: { id: bankAccountId },
+        lock: { mode: 'pessimistic_write' },
+      });
+
+      const transaction = manager.create(Transaction, {
+        ...createTransactionDto,
+        amount: createTransactionDto.amount * -1,
+        bank_account_id: bankAccountId,
+        operation: TransactionOperation.debit,
+      });
+
+      await manager.save(transaction);
+
+      bankAccount.balance += transaction.amount;
+      await manager.save(bankAccount);
+      return transaction;
+    });
+
+    const sendData = {
+      id: transaction.id,
+      accountId: bankAccountId,
+      amount: createTransactionDto.amount,
+      pixKeyTo: createTransactionDto.pix_key_key,
+      pixKeyKindTo: createTransactionDto.pix_key_kind,
+      description: createTransactionDto.description,
+    };
+
+    // A API do serviço devolve um Observable
+    // lastValueFrom transforma em Promise
+    await lastValueFrom(this.kafkaService.emit('transactions', sendData));
+    return transaction;
   }
 
-  findAll() {
-    return `This action returns all transactions`;
+  findAll(bankAccountId: string) {
+    return this.transactionRepo.find({
+      where: { bank_account_id: bankAccountId },
+      order: { created_at: 'DESC' },
+    });
   }
 
   findOne(id: number) {
@@ -29,7 +89,10 @@ export class TransactionsService {
   async confirmTransaction(arg0: ConfirmTransactionDto) {
     throw new Error('Method not implemented.');
   }
-  async createFromAnotherBankAccount(arg0: CreateTransactionFromAnotherBankAccountDto) {
-    throw new Error('Method not implemented.');
+
+  async createFromAnotherBankAccount(
+    input: CreateTransactionFromAnotherBankAccountDto
+  ) {
+    
   }
 }
